136e96b809e6cf29b839746c6b111abe36854ea4
dinotongo <dinotongo@gmail.com>
2026-02-20 23:19:05 +0700

    update

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 import ProductDetail from "@/components/Product/ProductDetail";
 import { products } from "@/data/products";
+import { useState } from "react";
 
 export default function Products() {
+  const [searchText, setSearchText] = useState("");
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-semibold mb-4">Our Cats</h1>
@@ -11,9 +13,12 @@ export default function Products() {
           type="text"
           placeholder="Search cats..."
           className="w-full px-4 py-2 rounded-md border focus:outline-none focus:ring focus:border-blue-300"
-          value={""}
-          onChange={(e) => console.log(e.target.value)}
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
         />
+      </div>
+      <div className="mb-4">
+        <p className="text-lg font-medium">Result for: {searchText}</p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {products.map((product, index) => (
